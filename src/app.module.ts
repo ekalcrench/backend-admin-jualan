889cb23234d5986759b6eeba8ccb8f72534728d9
ekalcrench <hhaekalazmi@gmail.com>
2026-08-11 20:56:30 +0700

    remove app controller yeay

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller.js';
-import { AppService } from './app.service.js';
 import { PrismaModule } from './prisma/prisma.module.js';
 import { UserModule } from './user/user.module.js';
 
 @Module({
   imports: [PrismaModule, UserModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
